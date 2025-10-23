99d18a606e56e9884ee1ed8eeb39838887eb87aa
leonidh <leonid.hiller@gmail.com>
2025-10-23 10:58:49 -0400

    docker

--- cmd/kv-service/main.go
+++ cmd/kv-service/main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/censys-sample/internal/app/kv-service/server"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"google.golang.org/grpc"
 
@@ -14,14 +17,22 @@ import (
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("[main] failed to listen: %v", err)
 	}
 
 	srv := grpc.NewServer()
 	store := kvstore.NewInMemoryStore()
 	pb.RegisterKVStoreServer(srv, server.NewKVServer(store))
-	log.Printf("gRPC server listening on :50051")
+	log.Printf("[main] gRPC server listening on :50051")
 	if err = srv.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("[main] failed to serve: %v", err)
 	}
+
+	terminate := make(chan os.Signal, 1)
+	signal.Notify(terminate, os.Interrupt, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+
+	log.Printf("[main] login service is up and running...")
+
+	sig := <-terminate
+	log.Printf("[main] registered signal='%s', shutting down...", sig.String())
 }
